bc6effbedd82df7ff64f2656d5631250234e1b50
Ben Niu <niuben003@gmail.com>
2014-11-17 11:50:00 -0500

    arch_prctl should not be allowed to the application.

--- lib/musl-1.0.4/src/linux/arch_prctl.c
+++ lib/musl-1.0.4/src/linux/arch_prctl.c
@@ -2,6 +2,7 @@
 #ifdef SYS_arch_prctl
 int arch_prctl(int code, unsigned long addr)
 {
-	return syscall(SYS_arch_prctl, code, addr);
+  //return syscall(SYS_arch_prctl, code, addr);
+  return -1;
 }
 #endif
