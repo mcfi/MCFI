f0d3d3f0ace067ff8f679020138c05b096e80065
Ben Niu <niuben003@gmail.com>
2014-11-20 20:48:26 -0500

    vfork.s directly invokes a system call that should be appropriately instrumented.

--- lib/musl-1.0.4/src/process/x86_64/vfork.s
+++ lib/musl-1.0.4/src/process/x86_64/vfork.s
@@ -6,7 +6,12 @@ __vfork:
 vfork:
 	pop %rdx
 	mov $58,%eax
+        movb $0x1, %fs:0x18 # raise the in-syscall flag 
 	syscall
+        movq %fs:0x10, %r11
+        addq $1, %r11
+        movq %r11, %fs:0x10
+        movb $0x0, %fs:0x18
 	push %rdx
 	mov %rax,%rdi
 	jmp __syscall_ret
