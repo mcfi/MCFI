a40f9c5d4169e9fef6ab36f2af517de0890fe0b4
Ben Niu <niuben003@gmail.com>
2014-11-20 20:39:54 -0500

    Atomic instructions are appropriately sandboxed by prefixing 0x67.

--- lib/musl-1.0.4/arch/x86_64/atomic.h
+++ lib/musl-1.0.4/arch/x86_64/atomic.h
@@ -17,74 +17,74 @@ static inline int a_ctz_l(unsigned long x)
 
 static inline void a_and_64(volatile uint64_t *p, uint64_t v)
 {
-	__asm__( "lock ; and %1, %0"
+	__asm__( "lock ; .byte 0x67\n\tand %1, %0"
 			 : "=m"(*p) : "r"(v) : "memory" );
 }
 
 static inline void a_or_64(volatile uint64_t *p, uint64_t v)
 {
-	__asm__( "lock ; or %1, %0"
+	__asm__( "lock ; .byte 0x67\n\tor %1, %0"
 			 : "=m"(*p) : "r"(v) : "memory" );
 }
 
 static inline void a_store_l(volatile void *p, long x)
 {
-	__asm__( "mov %1, %0" : "=m"(*(long *)p) : "r"(x) : "memory" );
+	__asm__( ".byte 0x67\n\tmov %1, %0" : "=m"(*(long *)p) : "r"(x) : "memory" );
 }
 
 static inline void a_or_l(volatile void *p, long v)
 {
-	__asm__( "lock ; or %1, %0"
+	__asm__( "lock ; .byte 0x67\n\tor %1, %0"
 		: "=m"(*(long *)p) : "r"(v) : "memory" );
 }
 
 static inline void *a_cas_p(volatile void *p, void *t, void *s)
 {
-	__asm__( "lock ; cmpxchg %3, %1"
+	__asm__( "lock ; .byte 0x67\n\tcmpxchg %3, %1"
 		: "=a"(t), "=m"(*(long *)p) : "a"(t), "r"(s) : "memory" );
 	return t;
 }
 
 static inline long a_cas_l(volatile void *p, long t, long s)
 {
-	__asm__( "lock ; cmpxchg %3, %1"
+	__asm__( "lock ; .byte 0x67\n\tcmpxchg %3, %1"
 		: "=a"(t), "=m"(*(long *)p) : "a"(t), "r"(s) : "memory" );
 	return t;
 }
 
 static inline int a_cas(volatile int *p, int t, int s)
 {
-	__asm__( "lock ; cmpxchg %3, %1"
+	__asm__( "lock ; .byte 0x67\n\tcmpxchg %3, %1"
 		: "=a"(t), "=m"(*p) : "a"(t), "r"(s) : "memory" );
 	return t;
 }
 
 static inline void *a_swap_p(void *volatile *x, void *v)
 {
-	__asm__( "xchg %0, %1" : "=r"(v), "=m"(*(void **)x) : "0"(v) : "memory" );
+	__asm__( ".byte 0x67\n\txchg %0, %1" : "=r"(v), "=m"(*(void **)x) : "0"(v) : "memory" );
 	return v;
 }
 static inline long a_swap_l(volatile void *x, long v)
 {
-	__asm__( "xchg %0, %1" : "=r"(v), "=m"(*(long *)x) : "0"(v) : "memory" );
+	__asm__( ".byte 0x67\n\txchg %0, %1" : "=r"(v), "=m"(*(long *)x) : "0"(v) : "memory" );
 	return v;
 }
 
 static inline void a_or(volatile void *p, int v)
 {
-	__asm__( "lock ; or %1, %0"
+	__asm__( "lock ; .byte 0x67\n\tor %1, %0"
 		: "=m"(*(int *)p) : "r"(v) : "memory" );
 }
 
 static inline void a_and(volatile void *p, int v)
 {
-	__asm__( "lock ; and %1, %0"
+	__asm__( "lock ; .byte 0x67\n\tand %1, %0"
 		: "=m"(*(int *)p) : "r"(v) : "memory" );
 }
 
 static inline int a_swap(volatile int *x, int v)
 {
-	__asm__( "xchg %0, %1" : "=r"(v), "=m"(*x) : "0"(v) : "memory" );
+	__asm__( ".byte 0x67\n\txchg %0, %1" : "=r"(v), "=m"(*x) : "0"(v) : "memory" );
 	return v;
 }
 
@@ -92,23 +92,23 @@ static inline int a_swap(volatile int *x, int v)
 
 static inline int a_fetch_add(volatile int *x, int v)
 {
-	__asm__( "lock ; xadd %0, %1" : "=r"(v), "=m"(*x) : "0"(v) : "memory" );
+	__asm__( "lock ; .byte 0x67\n\txadd %0, %1" : "=r"(v), "=m"(*x) : "0"(v) : "memory" );
 	return v;
 }
 
 static inline void a_inc(volatile int *x)
 {
-	__asm__( "lock ; incl %0" : "=m"(*x) : "m"(*x) : "memory" );
+	__asm__( "lock ; .byte 0x67\n\tincl %0" : "=m"(*x) : "m"(*x) : "memory" );
 }
 
 static inline void a_dec(volatile int *x)
 {
-	__asm__( "lock ; decl %0" : "=m"(*x) : "m"(*x) : "memory" );
+	__asm__( "lock ; .byte 0x67\n\tdecl %0" : "=m"(*x) : "m"(*x) : "memory" );
 }
 
 static inline void a_store(volatile int *p, int x)
 {
-	__asm__( "mov %1, %0" : "=m"(*p) : "r"(x) : "memory" );
+	__asm__( ".byte 0x67\n\tmov %1, %0" : "=m"(*p) : "r"(x) : "memory" );
 }
 
 static inline void a_spin()
