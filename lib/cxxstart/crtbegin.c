e35abad5b460603221bad684a08606dbbe721777
Ben Niu <niuben003@gmail.com>
2014-12-06 17:41:35 -0500

    Updated crtbegin.c to remove most code that has been implemented in musl libc.

--- lib/cxxstart/crtbegin.c
+++ lib/cxxstart/crtbegin.c
@@ -14,6 +14,9 @@ void *__dso_handle = &__dso_handle;
 void *__dso_handle = 0;
 #endif
 
+#if 0
+#ifdef SHARED
+
 extern void __cxa_finalize (void *) __attribute__((weak));
 
 static void __attribute__((used))
@@ -23,13 +26,22 @@ __do_global_dtors_aux(void) {
   if (__builtin_expect (completed, 0))
     return;
 
-#ifdef SHARED
   if (__cxa_finalize)
     __cxa_finalize (__dso_handle);
-#endif
+
   completed = 1;
 }
 
+__asm__ (".section .fini");
+__asm__ (".align 8, 0x90");
+__asm__ (".byte 0x0f, 0x1f, 0x00");
+__asm__ ("call __do_global_dtors_aux");
+#endif
+#endif
+/* The musl libc is responsible for init_array traversal so there is no
+   need to keep __do_global_ctors_aux
+*/
+#if 0
 static void __attribute__((used))
 __do_global_ctors_aux(void) {
   static int completed;
@@ -44,8 +56,4 @@ __asm__ (".section .init");
 __asm__ (".align 8, 0x90");
 __asm__ (".byte 0x0f, 0x1f, 0x00");
 __asm__ ("call __do_global_ctors_aux");
-
-__asm__ (".section .fini");
-__asm__ (".align 8, 0x90");
-__asm__ (".byte 0x0f, 0x1f, 0x00");
-__asm__ ("call __do_global_dtors_aux");
+#endif
