5c0b3390a4148002a1e8a4e5115381f8d641d740
ben <niuben003@gmail.com>
2015-04-24 08:12:41 -0400

    Align _init and _fini.

--- lib/musl-1.0.4/crt/x86_64/crti.s
+++ lib/musl-1.0.4/crt/x86_64/crti.s
@@ -1,11 +1,13 @@
         .section .init
         .type _init,@function
         .global _init
+        .align 16, 0x90
 _init:
 	push %rax
 
         .section .fini
         .type _fini,@function
         .global _fini
+        .align 16, 0x90
 _fini:
 	push %rax
