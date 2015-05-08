00bffdc88703d1a6a902aa81a997b1070e1138a4
Ben Niu <niuben003@gmail.com>
2015-05-08 06:08:30 -0400

    Fixed all missing cfg edges, afaik.

--- porting/nginx-1.4.0/nginx.sh
+++ porting/nginx-1.4.0/nginx.sh
@@ -11,6 +11,6 @@
 MCFI="`dirname ~`/`basename ~`/MCFI"
 PREFIX="$MCFI/server"
 
-./configure --with-cc=$MCFI/toolchain/bin/clang --with-cc-opt="-O2" --with-zlib=./zlib-1.2.8 --with-ipv6  --with-http_realip_module --with-http_addition_module --with-http_gunzip_module --with-http_gzip_static_module --with-http_sub_module --with-http_random_index_module --with-http_secure_link_module --with-http_degradation_module --with-http_stub_status_module --with-pcre=./pcre-8.37 --with-mail --prefix="$PREFIX"
+./configure --with-cc=$MCFI/toolchain/bin/clang --with-cc-opt="-O2" --with-zlib=./zlib-1.2.8 --with-http_spdy_module --with-ipv6  --with-http_realip_module --with-http_addition_module --with-http_gunzip_module --with-http_gzip_static_module --with-http_sub_module --with-http_random_index_module --with-http_secure_link_module --with-http_degradation_module --with-http_stub_status_module --with-pcre=./pcre-8.37 --with-mail --prefix="$PREFIX"
 
 make install
