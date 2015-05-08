045d7d23026ef66e6cf2b4ef2cbdaf85f91ed284
ben <niuben003@gmail.com>
2015-05-07 22:23:49 -0400

    Configured nginx with more features, which require code changes in the patch.

--- porting/nginx-1.4.0/nginx.sh
+++ porting/nginx-1.4.0/nginx.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# Apply nginx.patch to vanilla nginx-1.4.0
-# cp nginx.sh in nginx-1.4.0 and chmod +x nginx.sh
-# ./nginx.sh
-# nginx will be installed to $PREFIX/
-# sudo ./nginx and try accessing http://localhost
+# Steps to compile nginx
+# 1. download nginx-1.4.0 and decompress it
+# 2. apply nginx.patch to nginx-1.4.0
+# 3. download zlib-1.2.8 and decompress it to nginx-1.4.0
+# 4. copy this script to nginx-1.4.0 and set MCFI and PREFIX below
+# 5. exeucte this script, and you will find the nginx executable in $PREFIX/sbin/
 
-MCFI_SDK_PATH=
-PREFIX=~/MCFI/server
-CC=$MCFI_SDK_PATH/bin/clang
+MCFI="`dirname ~`/`basename ~`/MCFI"
+PREFIX="$MCFI/server"
 
-./configure --with-cc=$CC --without-pcre --without-http_rewrite_module --without-http_gzip_module --with-cc-opt="-O2" --prefix=/home/ben/MCFI/server --sbin-path=$PREFIX/nginx --conf-path=$PREFIX/conf --error-log-path=$PREFIX/error --pid-path=$PREFIX/pid --lock-path=$PREFIX/lock
+./configure --with-cc=$MCFI/toolchain/bin/clang --with-cc-opt="-O2" --with-zlib=./zlib-1.2.8 --with-ipv6  --with-http_realip_module --with-http_addition_module --with-http_sub_module --with-http_random_index_module --with-http_secure_link_module --with-http_degradation_module --with-http_stub_status_module --without-pcre --without-http_rewrite_module --with-mail --prefix="$PREFIX"
 
 make install
